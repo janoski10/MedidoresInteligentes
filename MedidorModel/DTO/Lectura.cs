02d0c7ca8ee7a291e1cac50004017acfa4303bf7
janoski10 <alejandro.acuna09@inacapmail.cl>
2022-06-20 19:49:36 -0400

    asignar caracteristicas a DAL y DTO

---
+++ MedidorModel/DTO/Lectura.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorModel.DTO
+{
+    public class Lectura
+    {
+        private Medidor numMedidor;
+        private DateTime fecha;
+        private DateTime hora;
+        private Medidor valorCon;
+
+        public Medidor NumMedidor { get => numMedidor; set => numMedidor = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public DateTime Hora { get => hora; set => hora = value; }
+        public Medidor ValorCon { get => valorCon; set => valorCon = value; }
+
+        public override string ToString()
+        {
+            return numMedidor + "| " + Fecha + "| " + valorCon;
+        }
+    }
+}
---
+++ MedidorModel/DTO/Medidor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidorModel.DTO
+{
+    public class Medidor
+    {
+        private int numMedidor;
+        private int tipo;
+        private Lectura valorCon;
+
+        private string tipoTxt
+        {
+            get
+            {
+                string tipoTxt = "";
+                switch (tipo)
+                {
+                    case 1: tipoTxt = "Inteligente";
+                        break;
+                    case 2: tipoTxt = "Antiguo";
+                        break;
+                }
+                return tipoTxt;
+            }
+        }
+        public int NumMedidor { get => numMedidor; set => numMedidor = value; }
+        public int Tipo { get => tipo; set => tipo = value; }
+        public Lectura ValorCon { get => valorCon; set => valorCon = value; }
+        
+    }
+}
